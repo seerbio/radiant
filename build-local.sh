8a4ea803acb8947904af4738db23ddf91f54bcf6
Seth Just <sjust@seer.bio>
2023-10-11 19:13:35 +0000

    Add scripts to run local build/test steps

---
+++ build-local.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# build-local.sh
+#
+# Build Pythia in the current directory.
+#
+# FOR DEVELOPMENT PURPOSES ONLY!!
+#
+# Assumes that dependencies were installed using the
+# default configuration of the install/get/build scripts.
+# For more information see `CONTRIBUTING.md`.
+
+# "Strict mode"
+set -euo pipefail
+
+cmake/bin/cmake -S . -B build/ -DPYTORCH_PREFIX_PATH=$(realpath ./pytorch)
+make -C build/ -j
