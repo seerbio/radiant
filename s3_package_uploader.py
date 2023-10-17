f88d6468dfc92e9dd3df9dc962f70643e9c2d53f
Seth Just <sjust@seer.bio>
2023-10-17 15:03:11 -0700

    Alter .deb naming scheme for upload to S3

--- s3_package_uploader.py
+++ s3_package_uploader.py
@@ -22,17 +22,14 @@ my_bucket = s3.Bucket(bucket_name)
 
 # Locate the local package file
 
-local_package: str = f"{package_name}-{arch}.deb"
+local_package: str = f"{package_name}+{arch}.deb"
 
 assert os.path.exists(local_package), f"Did not find local package {local_package}"
 print(f"Found package at {local_package}")
 
 # Construct the final filename for the uploaded package
-# TODO: pick final filenaming conv.
-#now: str = str(time.time())
-now = "test"
 
-upload_package_name: str = f"{now}_{package_name}-{arch}.deb"
+upload_package_name: str = f"{package_name}+{arch}.deb"
 
 print(f"Will upload {local_package} to s3://{bucket_name}/{upload_package_name}")
 
