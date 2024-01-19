4a93ce62b7273a86881eb1cb1d6820a100ba01ee
Drew Nichols <anichols@seer.bio>
2024-01-18 20:31:21 -0800

    ms2Ions docs

--- AlgorithmsFFLib/src/MS2Ion.h
+++ AlgorithmsFFLib/src/MS2Ion.h
@@ -30,22 +30,60 @@ public:
         return dbg;
     }
 
-    /*!
-    * @brief Returns the index (1 based, not 0) and the type of ion from ionLabel
-     * NOTE: An index of 0 is used to denote precursor.
+    /**
+    * @brief Gets ion label information for an MS2 ion.
+    *
+    * This function extracts and returns information about the ion label, including the ion index and type.
+    *
+    * @param ionInfo Output parameter, a QPair<IonIndex, IonType>* containing the ion index and type.
+    * @return An Err enum indicating the success or failure of the operation.
+    *
+    * NOTE: the index (1 based, not 0) and the type of ion from ionLabel
+    * NOTE: An index of 0 is used to denote precursor.
     */
     Err getIonLabelInfo(QPair<IonIndex, IonType> *ionInfo) const;
 
+    /**
+    * @brief Sorts MS2 ions in ascending order based on their mz values.
+    *
+    * This function sorts the provided QVector of MS2 ions in ascending order based on their mz values.
+    *
+    * @param ms2Ions A QVector<MS2Ion>* representing the MS2 ions to be sorted.
+    */
     static void sortMS2IonsMzAsc(QVector<MS2Ion> *ms2Ions);
 
+    /**
+    * @brief Sorts MS2 ions in descending order based on their intensity values.
+    *
+    * This function sorts the provided QVector of MS2 ions in descending order based on their intensity values.
+    *
+    * @param ms2Ions A QVector<MS2Ion>* representing the MS2 ions to be sorted.
+    */
     static void sortMS2IonsIntensityDesc(QVector<MS2Ion> *ms2Ions);
 
+    /**
+    * @brief Filters MS2 ions based on mz values within a specified range.
+    *
+    * This function filters the provided QVector of MS2 ions based on mz values within the specified range.
+    *
+    * @param mzMin A double representing the minimum mz value for filtering.
+    * @param mzMax A double representing the maximum mz value for filtering.
+    * @param ms2Ions A QVector<MS2Ion>* representing the MS2 ions to be filtered.
+    */
     static void filterMS2IonsByMz(
         double mzMin,
         double mzMax,
         QVector<MS2Ion> *ms2Ions
     );
 
+    /**
+    * @brief Converts MS2 ions to ScanPoints.
+    *
+    * This function converts a QVector of MS2 ions to ScanPoints, where each MS2Ion is converted to a ScanPoint.
+    *
+    * @param ms2Ions A QVector<MS2Ion> representing the MS2 ions to be converted.
+    * @return ScanPoints representing the converted ScanPoints.
+    */
     static ScanPoints ms2IonsToScanPoints(const QVector<MS2Ion> &ms2Ions);
 };
 
