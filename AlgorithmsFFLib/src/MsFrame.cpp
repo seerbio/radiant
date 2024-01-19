0866e9399129b50282e40041e9be4d09ab0260cb
Drew Nichols <anichols@seer.bio>
2024-01-18 22:58:18 -0800

    Finished MsFrame docs and test.

--- AlgorithmsFFLib/src/MsFrame.cpp
+++ AlgorithmsFFLib/src/MsFrame.cpp
@@ -131,6 +131,7 @@ Err MsFrame::init(
     ERR_INIT
 
     e = ErrorUtils::isNotEmpty(scanPoints); ree;
+    e = ErrorUtils::isEqual(scanPoints.size(), scanNumberVsScanTime.size()); ree;
 
     m_framePntrs = scanPoints;
 
