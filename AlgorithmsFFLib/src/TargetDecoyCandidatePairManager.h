2c4f43b99c383f19aebc210be07101d6c8fa3f43
Drew Nichols <anichols@seer.bio>
2024-01-19 20:57:51 -0800

    more docs

--- AlgorithmsFFLib/src/TargetDecoyCandidatePairManager.h
+++ AlgorithmsFFLib/src/TargetDecoyCandidatePairManager.h
@@ -28,21 +28,71 @@ public:
     TargetDecoyCandidatePairManager();
     ~TargetDecoyCandidatePairManager();
 
+    /**
+    * @brief Initializes the TargetDecoyCandidatePairManager.
+    *
+    * This method performs the following steps:
+    * 1. Checks if the PythiaParameters are valid.
+    * 2. Ensures that fragLibReaderRows is not empty.
+    * 3. Sets the PythiaParameters for the manager.
+    * 4. Removes non-sequences with non-canonical amino acids from fragLibReaderRows.
+    * 5. Builds target-decoy candidate pairs based on fragLibReaderRows.
+    * 6. Initializes the manager's private implementation with the generated candidate pairs.
+    *
+    * @param pythiaParameters The PythiaParameters to use for initialization.
+    * @param fragLibReaderRows A pointer to the QVector<FragLibReaderRow> containing fragment library reader rows.
+    * @return An error code indicating the success or failure of the initialization process.
+    */
     Err init(
             const PythiaParameters &pythiaParameters,
             QVector<FragLibReaderRow> *fragLibReaderRows
             );
 
+    /**
+    * @brief Checks whether the TargetDecoyCandidatePairManager has been successfully initialized.
+    *
+    * This method returns true if the list of target-decoy candidate pairs is not empty, indicating a successful initialization.
+    *
+    * @return True if the manager is initialized; false otherwise.
+    */
     bool isInit();
 
+    /**
+    * @brief Gets the total count of target-decoy candidate pairs managed by the TargetDecoyCandidatePairManager.
+    *
+    * @return The total count of target-decoy candidate pairs.
+    */
     int targetsCount();
 
+    /**
+    * @brief Retrieves pointers to TargetDecoyCandidatePair instances within a specified mz range.
+    *
+    * This method performs the following steps:
+    * 1. Checks if the list of target-decoy candidate pairs is not empty.
+    * 2. Ensures that mzMax is greater than or equal to mzMin.
+    * 3. Calls the private implementation to get pointers to candidate pairs within the specified mz range.
+    * 4. Shuffles the obtained pointers using a random number generator.
+    *
+    * @param mzMin The minimum mz value for filtering candidate pairs.
+    * @param mzMax The maximum mz value for filtering candidate pairs.
+    * @param targetDecoyPointers A pointer to a QVector<TargetDecoyCandidatePair*> to store the obtained pointers.
+    * @return An error code indicating the success or failure of the operation.
+    */
     Err getTargetDecoyCandidatePairPointers(
             double mzMin,
             double mzMax,
             QVector<TargetDecoyCandidatePair*> *targetDecoyPointers
             );
 
+    /**
+    * @brief Gets pointers to target-decoy candidate pairs within the specified mz range, with optional random selection.
+    *
+    * @param mzMin The minimum mz value.
+    * @param mzMax The maximum mz value.
+    * @param randomSelectionFraction The fraction of randomly selected pairs (use -1 for no random selection).
+    * @param targetDecoyPointers Pointer to a QVector to store the selected target-decoy candidate pair pointers.
+    * @return Error code indicating success or failure.
+    */
     Err getTargetDecoyCandidatePairPointers(
             double mzMin,
             double mzMax,
@@ -50,6 +100,14 @@ public:
             QVector<TargetDecoyCandidatePair*> *targetDecoyPointers
     );
 
+    /**
+    * @brief Extracts peptide string with modifications and charge from a peptide sequence charge key.
+    *
+    * @param peptideSequenceChargeKey The peptide sequence charge key to extract information from.
+    * @param peptideStringWithMods Pointer to store the extracted peptide string with modifications.
+    * @param charge Pointer to store the extracted charge.
+    * @return Error code indicating success or failure.
+    */
     static Err peptideStringWithModsFromPeptideSequenceChargeKey(
             const PeptideSequenceChargeKey &peptideSequenceChargeKey,
             PeptideStringWithMods *peptideStringWithMods,
--- AlgorithmsFFLib/src/TargetDecoyCandidatePairScoretron.h
+++ AlgorithmsFFLib/src/TargetDecoyCandidatePairScoretron.h
@@ -29,6 +29,20 @@ public:
     TargetDecoyCandidatePairScoretron();
     ~TargetDecoyCandidatePairScoretron() = default;
 
+    /**
+    * @brief Initializes the TargetDecoyCandidatePairScoretron.
+    *
+    * This method initializes the TargetDecoyCandidatePairScoretron with the provided parameters,
+    * including Pythia parameters, MS1 scan information, MS reader accessor, DIA target frames,
+    * and TurboXIC for MS1.
+    *
+    * @param pythiaParameters The Pythia parameters used for scoring.
+    * @param scanNumberVsScanTimeMS1 Map of scan numbers to MS1 scan points for the scoring reference.
+    * @param msReaderPointerAcc Pointer to the MS reader accessor for accessing MS data.
+    * @param diaTargetFrames Map of DIA target frames for additional scoring context.
+    * @param turboXICMS1 Pointer to TurboXIC for MS1, required for scoring calculations.
+    * @return Error code indicating success or failure during initialization.
+    */
     Err init(
             const PythiaParameters &pythiaParameters,
             const QMap<ScanNumber, ScanPoints> &scanNumberVsScanTimeMS1,
@@ -37,6 +51,21 @@ public:
             TurboXIC *turboXICMS1
             );
 
+    /**
+    * @brief Scores Target-Decoy pairs using parallel processing.
+    *
+    * This method scores Target-Decoy pairs based on specified parameters such as the number of top MS2 ions,
+    * scan time range, MS calibration information, map of MzTargetKeys to candidate pointers,
+    * and a vector to store the resulting candidate scores. The scoring is performed in parallel,
+    * leveraging available system processors for efficiency.
+    *
+    * @param topNMS2Ions Number of top MS2 ions to consider for scoring.
+    * @param scanTimeMinMax Pair representing the minimum and maximum scan times for scoring.
+    * @param msCalibratomatic MS calibration information.
+    * @param mzTargetKeyVsTargetDecoyCandidatePointers Map of MzTargetKeys to vectors of Target-Decoy candidate pointers.
+    * @param candidateScoresVec Vector to store the resulting candidate scores.
+    * @return Error code indicating success or failure during the scoring process.
+    */
     Err scoreTargetDecoyPairs(
             int topNMS2Ions,
             const QPair<ScanTime, ScanTime> &scanTimeMinMax,
@@ -45,8 +74,24 @@ public:
             QVector<CandidateScores> *candidateScoresVec
             );
 
+    /**
+    * @brief Checks if the TargetDecoyCandidatePairScoretron is initialized.
+    *
+    * This method checks whether the necessary components for scoring, including Pythia parameters,
+    * DIA target frames, and MS1 frame, are initialized.
+    *
+    * @return True if initialized, false otherwise.
+    */
     bool isInit();
 
+    /**
+    * @brief Sets the Pythia parameters for TargetDecoyCandidatePairScoretron.
+    *
+    * This method sets the Pythia parameters and checks if they are valid.
+    *
+    * @param pythiaParameters The Pythia parameters to set.
+    * @return An error code indicating success or failure.
+    */
     Err setPythiaParameters(const PythiaParameters &pythiaParameters);
 
 private:
