342b6d5971c223a7be478670019eb4eaad64d48d
Drew Nichols <anichols@seer.bio>
2024-07-17 12:20:14 -0700

    removed unnecessary vcpkg files.

--- install-build-deps-ubuntu.sh
+++ install-build-deps-ubuntu.sh
@@ -50,7 +50,7 @@ git clone https://github.com/microsoft/vcpkg.git
 ./vcpkg/bootstrap-vcpkg.sh
 ./vcpkg/vcpkg install aws-sdk-cpp
 ./vcpkg/vcpkg install utf8proc
-./vcpkg/vcpkg install lz4
-./vcpkg/vcpkg install zstd
-./vcpkg/vcpkg install re2
+#./vcpkg/vcpkg install lz4
+#./vcpkg/vcpkg install zstd
+#./vcpkg/vcpkg install re2
 ./vcpkg/vcpkg integrate install
