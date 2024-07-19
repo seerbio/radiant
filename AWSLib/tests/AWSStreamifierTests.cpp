581653508698b9a1169a4ba7ebc02d53dccebc88
Drew Nichols <anichols@seer.bio>
2024-07-19 14:06:11 -0700

    Switched to env vars

--- AWSLib/tests/AWSStreamifierTests.cpp
+++ AWSLib/tests/AWSStreamifierTests.cpp
@@ -4,6 +4,7 @@
 
 #include "AWSStreamifier.h"
 
+#include <QProcessEnvironment>
 #include <QtTest/QtTest>
 
 class AWSStreamifierTests : public QObject
@@ -23,17 +24,21 @@ private Q_SLOTS:
 
 void AWSStreamifierTests::streamTextFileTest() {
 
-    QSKIP("temp");
+    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
 
-    const QString accessKeyId = "ASIA2OXTCKO6AFXJIBOJ";
-    const QString secretAccessKey = "nyAGcu1/gHDBqypr2UZQVl1RNTa5VA8joOz4hC+E";
-    const QString accessToken = "IQoJb3JpZ2luX2VjEFsaCXVzLXdlc3QtMiJGMEQCICxWGVgGZSdqDtcHBpIh5zyHB3xNjOvNjQ5w0t8W+n8pAiAGt/oyjpFGa2LFjKI8vsNCpngZaFpplDl5aHWkVLMc9SqMAwg0EAUaDDcxODg0MzA0MDcwMCIMjQq949PgLVNtnPTKKukCES+e7JHJST1dkSowNU0HbRbe0jtEk8lViyUNqTvEhvo5Z/Bs7RDLgX7XoWT6Wnmcx2creKM9PTMUP8Yi7HrIC6jxCG1DQeJpxllwFd3dT1KEk0rs4VKrFkDXeP3cmwIHhGuoQC9EM6tDfYG2uMtskVIdIOQ18DJvLdI4UksqIC4iZu1ADF/6ctOmVUnOeSmhM9eGHtHd3mrPiw15wSG/gQFYQ5rUy+vbO4yCm1kzSUP9aVxkZpl6K6uMlv1d38fGhbe9OIZncj7Ozt1CuquP9XJBV1DnTEl9YDmNjii+3Ck5+t429tJ3rES3QHBxr4a1Somt6ZoSysgnyogr5qbNrfD80EmEa/werYG9edIy6fGc+r5SAe10k/AfzOO6nHtSDKVzUquRwEmHxls/vXAWz1LTjJuQ5Zap1YcSW2Ob7w+u6H0rNwRC1xgNJIT8+4kLgywKToiCbFVh9R1OXp8C3LyBDCE+CoYH/DD45+q0BjqnAc8Qa9F7FlWe7NR6Wz1+/RzH11O5gBQ7mLYd+oW3WhEwR0TRndRRD9X812sSK43/3gQG7sEDNXrHYRIcCzoDcu3wayGa8KXTiLbMHIXwrtmBa3kiPX54HvNGCPPceMYhWoozejRMgxMWsLSN8RclngQDy9Fhy6gvCm5r2qYghZk0L02xBRpECpfo+x8ZGZ5/adpuAh2etw8bVTmeVlN+cTJFwOHWaA4S";
+    if(
+        !(env.contains("AWS_ACCESS_KEY_ID")
+        && env.contains("AWS_SECRET_ACCESS_KEY")
+        && env.contains("AWS_SESSION_TOKEN"))
+        ) {
+        QSKIP("Skipping due to no credentials provided");
+    }
 
     AWSStreamifier awsStreamifier;
     const bool credentialsSet = awsStreamifier.setAWSCredentials(
-        accessKeyId,
-        secretAccessKey,
-        accessToken
+        env.value("AWS_ACCESS_KEY_ID"),
+        env.value("AWS_SECRET_ACCESS_KEY"),
+        env.value("AWS_SESSION_TOKEN")
         );
 
     const QString uri = "s3://seer-experiments/EXP24001/mzml/EXP24001_2023ms1005bX1_A.raw.mzML";
@@ -45,17 +50,21 @@ void AWSStreamifierTests::streamTextFileTest() {
 
 void AWSStreamifierTests::streamParquetFileTest() {
 
-    // QSKIP("temp");
+    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
 
-    const QString accessKeyId = "ASIA2OXTCKO6AFXJIBOJ";
-    const QString secretAccessKey = "nyAGcu1/gHDBqypr2UZQVl1RNTa5VA8joOz4hC+E";
-    const QString accessToken = "IQoJb3JpZ2luX2VjEFsaCXVzLXdlc3QtMiJGMEQCICxWGVgGZSdqDtcHBpIh5zyHB3xNjOvNjQ5w0t8W+n8pAiAGt/oyjpFGa2LFjKI8vsNCpngZaFpplDl5aHWkVLMc9SqMAwg0EAUaDDcxODg0MzA0MDcwMCIMjQq949PgLVNtnPTKKukCES+e7JHJST1dkSowNU0HbRbe0jtEk8lViyUNqTvEhvo5Z/Bs7RDLgX7XoWT6Wnmcx2creKM9PTMUP8Yi7HrIC6jxCG1DQeJpxllwFd3dT1KEk0rs4VKrFkDXeP3cmwIHhGuoQC9EM6tDfYG2uMtskVIdIOQ18DJvLdI4UksqIC4iZu1ADF/6ctOmVUnOeSmhM9eGHtHd3mrPiw15wSG/gQFYQ5rUy+vbO4yCm1kzSUP9aVxkZpl6K6uMlv1d38fGhbe9OIZncj7Ozt1CuquP9XJBV1DnTEl9YDmNjii+3Ck5+t429tJ3rES3QHBxr4a1Somt6ZoSysgnyogr5qbNrfD80EmEa/werYG9edIy6fGc+r5SAe10k/AfzOO6nHtSDKVzUquRwEmHxls/vXAWz1LTjJuQ5Zap1YcSW2Ob7w+u6H0rNwRC1xgNJIT8+4kLgywKToiCbFVh9R1OXp8C3LyBDCE+CoYH/DD45+q0BjqnAc8Qa9F7FlWe7NR6Wz1+/RzH11O5gBQ7mLYd+oW3WhEwR0TRndRRD9X812sSK43/3gQG7sEDNXrHYRIcCzoDcu3wayGa8KXTiLbMHIXwrtmBa3kiPX54HvNGCPPceMYhWoozejRMgxMWsLSN8RclngQDy9Fhy6gvCm5r2qYghZk0L02xBRpECpfo+x8ZGZ5/adpuAh2etw8bVTmeVlN+cTJFwOHWaA4S";
+    if(
+        !(env.contains("AWS_ACCESS_KEY_ID")
+        && env.contains("AWS_SECRET_ACCESS_KEY")
+        && env.contains("AWS_SESSION_TOKEN"))
+        ) {
+        QSKIP("Skipping due to no credentials provided");
+        }
 
     AWSStreamifier awsStreamifier;
     const bool credentialsSet = awsStreamifier.setAWSCredentials(
-        accessKeyId,
-        secretAccessKey,
-        accessToken
+        env.value("AWS_ACCESS_KEY_ID"),
+        env.value("AWS_SECRET_ACCESS_KEY"),
+        env.value("AWS_SESSION_TOKEN")
         );
 
     const QString uri = "s3://seer-experiments/EXP24039/interimresult/0b6d21d0b237173e9ed0ec96aea642cd/500873.EXP24039_2024ms0407dX45_A.raw.mzML.pythiaDIA";
