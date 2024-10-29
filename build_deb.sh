0d4ea94ba2ee39cc2c2ed29b295896333637f9bf
Drew Nichols <anichols@seer.bio>
2024-10-29 13:58:01 -0700

    fixed tims dependency correctly this time.

--- build_deb.sh
+++ build_deb.sh
@@ -41,7 +41,7 @@ cp FileReadersLib/libFileReadersLib.so "$pythia_lib/libFileReadersLib.so"
 cp UtilsLib/libUtilsLib.so "$pythia_lib/libUtilsLib.so"
 cp WorkFlowsLib/libWorkFlowsLib.so "$pythia_lib/libWorkFlowsLib.so"
 cp PyTorchLib/libPyTorchLib.so "$pythia_lib/libPyTorchLib.so"
-cp ThirdPartyLibs/timsdata-2.21.0.4/timsdata/linux64/libtimsdata.so "$pythia_lib/libtimsdata.so"
+cp /src/PythiaDIACpp/ThirdPartyLibs/timsdata-2.21.0.4/timsdata/linux64/libtimsdata.so "$pythia_lib/libtimsdata.so"
 
 cp /src/pytorch/build/lib/* "$pythia_lib/"
 
