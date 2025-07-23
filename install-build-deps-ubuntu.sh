69eb3b22af3eb30fc137be7c1c61e1b495edc7b0
Seth Just <sjust@seer.bio>
2025-07-23 15:20:17 -0700

    🔨 Improve build scripts with suggestions from review

--- install-build-deps-ubuntu.sh
+++ install-build-deps-ubuntu.sh
@@ -7,6 +7,9 @@
 # If unset, use `sudo apt-get` as the apt command
 APT=${APT:-'sudo apt-get'}
 
+# The version of CMake to install; must be > 3.17!
+CMAKE_VERSION=${CMAKE_VERSION:-'3.23.2'}
+
 # If unset, install `cmake` into the current directory
 CMAKE_PREFIX=${CMAKE_PREFIX:-'./cmake'}
 
@@ -16,12 +19,15 @@ set -euo pipefail
 # Get initial requirements
 ${APT} install --no-install-recommends -y ca-certificates lsb-release wget
 
-# Allow overriding the distribution name -- on Pop!OS run with `DISTRO=`
-# Fallback to using lsb_release only occurs when DISTRO is not set; if blank we will fall back to default below.
+# Allow overriding the distribution name -- on Pop!OS run with `DISTRO=`.
+# If DISTRO is unset, use `lsb_release` to determine the distribution name.
+# If DISTRO is set to an empty value or `lsb_release` returns nothing, fall back to the default 'ubuntu'.
 DISTRO=${DISTRO-"$(lsb_release --id --short | tr '[:upper:]' '[:lower:]')"}
+DISTRO=${DISTRO:-ubuntu}
+
 CODENAME=$(lsb_release --codename --short)
 
-wget -P /tmp/ https://apache.jfrog.io/artifactory/arrow/${DISTRO:-ubuntu}/apache-arrow-apt-source-latest-${CODENAME}.deb
+wget -P /tmp/ "https://apache.jfrog.io/artifactory/arrow/${DISTRO}/apache-arrow-apt-source-latest-${CODENAME}.deb"
 ${APT} install -y -V "/tmp/apache-arrow-apt-source-latest-${CODENAME}.deb"
 rm "/tmp/apache-arrow-apt-source-latest-${CODENAME}.deb"
 ${APT} update
@@ -41,7 +47,7 @@ ${APT} install --no-install-recommends -y \
     libqt5svg5-dev
 
 # Install latest CMAKE > 3.17
-wget "https://github.com/Kitware/CMake/releases/download/v3.23.2/cmake-3.23.2-Linux-$(uname -m).sh" -q -O /tmp/cmake-install.sh
+wget "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-Linux-$(uname -m).sh" -q -O /tmp/cmake-install.sh
 chmod u+x /tmp/cmake-install.sh
 mkdir -p "${CMAKE_PREFIX}"
 /tmp/cmake-install.sh --skip-license --prefix="${CMAKE_PREFIX}"
