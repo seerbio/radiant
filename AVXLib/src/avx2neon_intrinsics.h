3aefba8b42ef7f73bfe813f22dc91690368e84a2
Seth Just <sjust@seer.bio>
2025-07-24 12:19:29 -0700

    🩹 Fix intrinsic impl (with Copilot)

--- AVXLib/src/avx2neon_intrinsics.h
+++ AVXLib/src/avx2neon_intrinsics.h
@@ -26,16 +26,34 @@ static inline void _mm256_storeu_ps(float* mem_addr, __m256 a) {
 // _mm256_unpacklo_epi16: Interleave lower 8 int16_t from each __m256i, zero-extend to int32_t
 static inline __m256i _mm256_unpacklo_epi16(__m256i a, __m256i b) {
     __m256i result;
-    result.vect_i128[0] = vreinterpretq_s64_s32(vmovl_s16(vget_low_s16(vreinterpretq_s16_s64(a.vect_i128[0]))));
-    result.vect_i128[1] = vreinterpretq_s64_s32(vmovl_s16(vget_low_s16(vreinterpretq_s16_s64(b.vect_i128[0]))));
+    // Interleave lower 4 elements of each 128-bit lane
+    int16x4_t a0 = vget_low_s16(a.vect_s16[0]);
+    int16x4_t b0 = vget_low_s16(b.vect_s16[0]);
+    int16x4_t a1 = vget_low_s16(a.vect_s16[1]);
+    int16x4_t b1 = vget_low_s16(b.vect_s16[1]);
+    // Interleave
+    int16x4x2_t zipped0 = vzip_s16(a0, b0);
+    int16x4x2_t zipped1 = vzip_s16(a1, b1);
+    // Widen to int32x4_t
+    result.vect_s32[0] = vmovl_s16(zipped0.val[0]);
+    result.vect_s32[1] = vmovl_s16(zipped1.val[0]);
     return result;
 }
 
 // _mm256_unpackhi_epi16: Interleave upper 8 int16_t from each __m256i, zero-extend to int32_t
 static inline __m256i _mm256_unpackhi_epi16(__m256i a, __m256i b) {
     __m256i result;
-    result.vect_i128[0] = vreinterpretq_s64_s32(vmovl_s16(vget_high_s16(vreinterpretq_s16_s64(a.vect_i128[0]))));
-    result.vect_i128[1] = vreinterpretq_s64_s32(vmovl_s16(vget_high_s16(vreinterpretq_s16_s64(b.vect_i128[0]))));
+    // Interleave upper 4 elements of each 128-bit lane
+    int16x4_t a0 = vget_high_s16(a.vect_s16[0]);
+    int16x4_t b0 = vget_high_s16(b.vect_s16[0]);
+    int16x4_t a1 = vget_high_s16(a.vect_s16[1]);
+    int16x4_t b1 = vget_high_s16(b.vect_s16[1]);
+    // Interleave
+    int16x4x2_t zipped0 = vzip_s16(a0, b0);
+    int16x4x2_t zipped1 = vzip_s16(a1, b1);
+    // Widen to int32x4_t
+    result.vect_s32[0] = vmovl_s16(zipped0.val[0]);
+    result.vect_s32[1] = vmovl_s16(zipped1.val[0]);
     return result;
 }
 
