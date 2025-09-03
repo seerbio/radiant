759221ed717d1c039fa72dc920f2afa83aada1cf
Drew Nichols <anichols@seer.bio>
2025-09-03 11:00:11 -0700

    Revert "WIP"
    This reverts commit 974e31fa678b0a975889ebb8ac9db3b27c5c4603.

--- AlgorithmsFFLib/src/Ms2IonFraggertronManager.h
+++ AlgorithmsFFLib/src/Ms2IonFraggertronManager.h
@@ -8,43 +8,9 @@
 #include "AlgorithmsFFLib_Exports.h"
 #include "MS2Ion.h"
 #include "MsReaderBase.h"
-#include "MsReaderPointerAcc.h"
-#include "PythiaParameterReader.h"
 #include "TargetDecoyCandidatePair.h"
 
 #include "Error.h"
-
-struct TallyResult {
-	ScanNumber scanNumber = -1;
-	bool isDecoy = false;
-	Occurrence occurrence = 0;
-	float cosineSimilarity = -1.0;
-	float intensitiesSum = -1.0;
-	QVector<int> ranks;
-	// int indexesFoundY = 0;
-	// int indexesFoundB = 0;
-	// int seqTagLongestY = 0;
-	// int seqTagLongestB = 0;
-};
-
-struct Tally {
-	ScanNumber scanNumber = -1;
-	Occurrence occurrence = 0;
-	float cosineSimilarity = -1.0;
-	float intensitiesSum = -1.0;
-	QVector<int> ranks;
-	QVector<float> intensitiesEmperical;
-	QVector<float> intensitiesTheoretical;
-	// int indexesFoundY = 0;
-	// int indexesFoundB = 0;
-	// int seqTagLongestY = 0;
-	// int seqTagLongestB = 0;
-};
-
-using TallyResultTarget = TallyResult;
-using TallyResultDecoy = TallyResult;
-using TallyResultTuple
-	= std::tuple<TargetDecoyCandidatePair*, QVector<TallyResultTarget>, QVector<TallyResultDecoy>>;
 
 using namespace Error;
 
@@ -84,38 +50,22 @@ public:
     ~Ms2IonFraggertronManager();
 
     [[nodiscard]] Err init(
-    	const PythiaParameters &parameters,
-		const QVector<MS2IonLibrary*> &ms2IonsLibrary,
-		MsReaderPointerAcc *msReaderPtr
-    	);
+		const QVector<MS2IonLibrary*> &ms2IonsLibrary
+    	) const;
 
-	Err performFragging();
+    Err extractMs2Points(
+        float mzMin,
+        float mzMax,
+        float iRTMin,
+        float iRTMax,
+        QVector<MS2IonLibrary*> *tdPeptideFrags
+        ) const;
 
 private:
-
-	Err buildMsScanPointPntrs(MsReaderPointerAcc *msReaderPtr);
-
-	Err extractScansParallel(
-		const QVector<MsScanInfo*> &scanInfosPntrs,
-		MsReaderPointerAcc *msReaderPtr
-		);
-
-	Err extractMs2Points(
-		float mzMin,
-		float mzMax,
-		float iRTMin,
-		float iRTMax,
-		QVector<MS2IonLibrary*> *tdPeptideFrags
-		) const;
 
     [[nodiscard]] Err initTesting(const QVector<CandidateScores*> &candidateScoresPntrs) const;
 
 private:
-
-	PythiaParameters m_parameters;
-	QVector<QVector<MsScanPoint>> m_msScanPointsTranched;
-	QVector<QVector<MsScanPoint*>> m_msScanPointsPntrsTranched;
-	QVector<QPair<float, float>> m_mzPrecursorStartVsStopResult;
 
     Q_DISABLE_COPY(Ms2IonFraggertronManager) class Private;
     const QScopedPointer<Private> d_ptr;
