6eac43f9fabde15e3d51fc7206e715dc2147302b
Drew Nichols <anichols@seer.bio>
2025-10-07 16:47:26 -0700

    (refactor): Adjust max counts for high-accuracy plateau in PPM optimization

--- WorkFlowsLib/src/PythiaDIAFFWorkflowAlgos/OptimizeMassAccuracyPPMSettertron.cpp
+++ WorkFlowsLib/src/PythiaDIAFFWorkflowAlgos/OptimizeMassAccuracyPPMSettertron.cpp
@@ -300,7 +300,7 @@ Err OptimizeMassAccuracyPPMSettertron::optimizePPM() {
             continue;
         }
 
-        constexpr int maxCountsSinceLastHigh = 3;
+        constexpr int maxCountsSinceLastHigh = 4;
         if (++countSinceLastHigh >= maxCountsSinceLastHigh) {
             break;
         }
