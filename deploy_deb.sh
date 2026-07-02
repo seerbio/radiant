e11376cbd73f79a8f94c3eefc95001f5b9d5d9ab
Seth Just <sjust@seer.bio>
2026-07-02 11:25:28 -0700

    🚚 Update publication to create .DEB packages and cache layers

--- deploy_deb.sh
+++ deploy_deb.sh
@@ -5,34 +5,31 @@ set -eu -o pipefail
 
 CONTAINER=${CONTAINER:-seer/radiant}
 VERSION=${VERSION:-$(./get_release_version.sh)}
+DEB_OUTPUT_DIR=${DEB_OUTPUT_DIR:?DEB_OUTPUT_DIR is required}
 export radiantdia_version=${radiantdia_version:-${VERSION}}
 
 echo "Found radiantdia version '${radiantdia_version}'"
 
 echo "Building deploy container…"
 DEPLOY_IMG="${CONTAINER}-deb-build:${radiantdia_version}"
-docker build --target build-deb --build-arg radiantdia_version -t "${DEPLOY_IMG}" .
+docker_build_args=(
+    build
+    --load
+    --target build-deb
+    --build-arg "radiantdia_version=${radiantdia_version}"
+    -t "${DEPLOY_IMG}"
+)
 
-# Run the default command from the `deploy` stage.
-# This builds the DEB and pushes it to S3.
-echo "Running deployment…"
-docker_run_args=(--rm)
+docker buildx "${docker_build_args[@]}" .
 
-if [[ -n "${DEB_OUTPUT_DIR:-}" ]]; then
-    mkdir -p "${DEB_OUTPUT_DIR}"
-    HOST_DEB_OUTPUT_DIR=$(realpath "${DEB_OUTPUT_DIR}")
-    HOST_UID=$(id -u)
-    HOST_GID=$(id -g)
+mkdir -p "${DEB_OUTPUT_DIR}"
+HOST_DEB_OUTPUT_DIR=$(realpath "${DEB_OUTPUT_DIR}")
+HOST_UID=$(id -u)
+HOST_GID=$(id -g)
 
-    echo "Will also copy the DEB to '${HOST_DEB_OUTPUT_DIR}'"
-    echo "Will run the uploader as uid:gid '${HOST_UID}:${HOST_GID}'"
-
-    docker_run_args+=(
-        --user "${HOST_UID}:${HOST_GID}"
-        -v "${HOST_DEB_OUTPUT_DIR}:/tmp/deb-out"
-        -e "HOME=/tmp"
-        -e "LOCAL_DEB_OUTPUT_DIR=/tmp/deb-out"
-    )
-fi
-
-docker run "${docker_run_args[@]}" "${DEPLOY_IMG}"
+echo "Exporting DEB artifacts…"
+docker run --rm \
+    --user "${HOST_UID}:${HOST_GID}" \
+    -v "${HOST_DEB_OUTPUT_DIR}:/tmp/deb-out" \
+    -e "LOCAL_DEB_OUTPUT_DIR=/tmp/deb-out" \
+    "${DEPLOY_IMG}"
--- deploy_docker_image.sh
+++ deploy_docker_image.sh
@@ -6,6 +6,7 @@ set -eu -o pipefail
 CONTAINER=${CONTAINER:-seer/radiant}
 REPOSITORY=${REPOSITORY:-docker.io}
 TAG_SUFFIX=${TAG_SUFFIX:?TAG_SUFFIX is required}
+CACHE_SCOPE=${CACHE_SCOPE:-}
 
 VERSION=${VERSION:-$(./get_release_version.sh)}
 
@@ -18,16 +19,25 @@ if [[ -n "${1:-}" ]]; then
 fi
 
 docker_build_args=(
+  build
+  --load
   --target app
   --build-arg "radiantdia_version=${VERSION}"
 )
+
+if [[ -n "${CACHE_SCOPE}" ]]; then
+  docker_build_args+=(
+    --cache-from "type=gha,scope=${CACHE_SCOPE}"
+    --cache-to "type=gha,scope=${CACHE_SCOPE},mode=max"
+  )
+fi
 
 for TAG in "${TAGS[@]}"; do
   docker_build_args+=(-t "${REPOSITORY}/${CONTAINER}:${TAG}")
 done
 
 echo "Building container image(s): ${TAGS[*]}"
-docker build "${docker_build_args[@]}" .
+docker buildx "${docker_build_args[@]}" .
 
 for TAG in "${TAGS[@]}"; do
   echo "Pushing ${REPOSITORY}/${CONTAINER}:${TAG}"
